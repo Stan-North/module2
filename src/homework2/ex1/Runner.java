7b7786678e296cbbae75291b66a90c488bceafbc
Stan-North <stanislavkulbitski@MacBook-Air-Stanislav.local>
2024-09-08 22:54:30 +0300

    ДЗ №2

---
+++ src/homework2/ex1/Runner.java
@@ -0,0 +1,77 @@
+package homework2.ex1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+
+
+public class Runner {
+    private static final String FILE_NAME = "resources/luggage.csv";
+    private static final int FEED_CAPACITY = 10;
+    private static final String UNLOADING_MESSAGE = "Лента загружена, начинается выдача багажа.";
+    private static final String FEED_IS_EMPTY = "Лента пустая, закончена выдача багажа";
+    private static final String UPLOAD_IS_STARTED = "Начинается загрузка на ленту";
+    private static final String SPLITTERATOR = ";";
+    private static final String LUGGAGE_GIVEN = " - выдан";
+    private static final int INDEX_OF_LUGGAGE_NUMBER = 0;
+
+    public static void main(String[] args) {
+        luggageUnload(FILE_NAME);
+    }
+
+    /**
+     * метод, который все запускает
+     */
+    public static void luggageUnload (String fileName) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            reader.readLine(); // пропускаем первую строку
+            while (reader.ready()) {
+                unloadFeed(uploadFeed(reader));
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * загрузка ленты
+     */
+    private static LinkedList<String> uploadFeed(BufferedReader reader) throws IOException {
+        int counter = 0;
+        LinkedList<String> feed = new LinkedList<>();
+        System.out.println(UPLOAD_IS_STARTED);
+
+        while (reader.ready()) {
+            if (counter == FEED_CAPACITY) {
+                return feed;
+            } else {
+                feed.add(reader.readLine());
+                counter++;
+            }
+        }
+        return feed;
+    }
+
+    /**
+     * определение номера багажа
+     */
+     private static String getNumberOfLuggage(String line) {
+         if (line != null) {
+             String[] splittedLine =  line.split(SPLITTERATOR);
+             return splittedLine[INDEX_OF_LUGGAGE_NUMBER];
+         }
+        return "";
+     }
+
+    /**
+     * разгрузка ленты
+     */
+    private static void unloadFeed(LinkedList<String> feed) {
+        System.out.println(UNLOADING_MESSAGE);
+        while (!feed.isEmpty()) {
+            System.out.println(getNumberOfLuggage(feed.poll()) + LUGGAGE_GIVEN);
+        }
+        System.out.println(FEED_IS_EMPTY + "\n");
+    }
+}
---
+++ src/homework2/ex2/Runner.java
@@ -0,0 +1,68 @@
+package homework2.ex2;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class Runner {
+    private static final int LIGHT_WEIGHT = 5;
+    private static final int HEAVY_WEIGHT = 10;
+    private static final String FILE_NAME = "resources/luggage.csv";
+    private static final String SPLITTERATOR = ";";
+    private static final int INDEX_OF_LUGGAGE_WEIGHT = 1;
+    private static final String LIGHT = "легкий";
+    private static final String MIDDLE = "средний";
+    private static final String HEAVY = "тяжелый";
+    private static final int INITIAL_VALUE = 0;
+
+    public static void main(String[] args) {
+        HashMap<String, Integer> result = calculateStatistics(FILE_NAME);
+        System.out.println(result);
+    }
+
+    /**
+     * основной метод
+     */
+    public static HashMap<String, Integer> calculateStatistics(String fileName) {
+        HashMap<String, Integer> map = new HashMap<>();
+        mapInitialize(map);
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            reader.readLine(); //пропускаем первую строку
+            while (reader.ready()) {
+                addWeightToMap(reader.readLine(), map);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return map;
+    }
+
+    /**
+     * Инициализация мапы и заполнение стартовыми значениями
+     */
+    public static void mapInitialize(HashMap<String, Integer> result) {
+        result.put(LIGHT, INITIAL_VALUE);
+        result.put(MIDDLE, INITIAL_VALUE);
+        result.put(HEAVY, INITIAL_VALUE);
+    }
+
+    /**
+     * добавление значений веса в мапу
+     */
+    public static void addWeightToMap(String line, HashMap<String, Integer> hashMap) {
+        if (line != null) {
+            String[] splittedLine =  line.split(SPLITTERATOR);
+            int value = Integer.parseInt(splittedLine[INDEX_OF_LUGGAGE_WEIGHT]);
+            if (value < LIGHT_WEIGHT) {
+                hashMap.computeIfPresent(LIGHT, (k, v) -> v + value);
+            } else if (value > LIGHT_WEIGHT && value < HEAVY_WEIGHT) {
+                hashMap.computeIfPresent(MIDDLE, (k, v) -> v + value);
+            } else {
+                hashMap.computeIfPresent(HEAVY, (k, v) -> v + value);
+            }
+        }
+
+    }
+
+}
