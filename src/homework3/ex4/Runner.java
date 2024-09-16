1afa0e32738f5431cc4bc774057ced01e174f72b
Stan-North <stanislavkulbitski@MacBook-Air-Stanislav.local>
2024-09-16 22:42:02 +0300

    ДЗ №3

--- src/homework3/ex4/Runner.java
+++ src/homework3/ex4/Runner.java
@@ -6,7 +6,7 @@ public class Runner {
         Ruler<Animal> animalRuler = animal -> animal.getBodyLength() + animal.getTailLength();
         Ruler<Boat> boatRuler = boat -> boat.getLength();
 
-        System.out.println(animalRuler.makeMeasure(new Animal(60,40)));
+        System.out.println(animalRuler.makeMeasure(new Animal(60, 40)));
         System.out.println(boatRuler.makeMeasure(new Boat(320)));
     }
 }
